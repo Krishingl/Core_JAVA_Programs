91308b04b3b6dcc5af27b3c33151e570d88d5568
HP <HP@DESKTOP-R9VPA0E>
2025-06-08 05:26:17 +0530

    first program done....

---
+++ Core_JAVA_Programs/src/com/krush/hello_devloper/HelloDevloper.java
@@ -0,0 +1,10 @@
+package com.krush.hello_devloper;
+
+public class HelloDevloper {
+	static{
+		  System.out.println("Hello Devloper from Static block ");
+	}
+public static void main(String[] args) {
+	 System.out.println("Hello Devloper from main_frame_satck");
+}
+}
