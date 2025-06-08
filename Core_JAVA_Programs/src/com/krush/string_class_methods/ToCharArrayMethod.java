40135507b8020d2fddd12b85dc880b846ab710b5
krrishnaingle2001.gmail.com <krishnaingle2001@gmail.com>
2025-06-08 16:34:54 +0530

    toSting method its available from Arrays class. its conver char array to String

--- Core_JAVA_Programs/src/com/krush/string_class_methods/ToCharArrayMethod.java
+++ Core_JAVA_Programs/src/com/krush/string_class_methods/ToCharArrayMethod.java
@@ -1,6 +1,6 @@
 package com.krush.string_class_methods;
 
-
+import java.util.Arrays;
 public class ToCharArrayMethod {
 	
 	
@@ -20,6 +20,9 @@ for(char character : characters )
 {
 	System.out.print(character+",");//H,e,l,l,o, ,W,o,r,l,d,
 }
-		
+	
+System.out.println("\n----------------------------------");
+
+System.out.println(Arrays.toString(characters));//toString is use to convert character  to string format
 	}
 }
