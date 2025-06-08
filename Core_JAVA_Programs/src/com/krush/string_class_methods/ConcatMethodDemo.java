d0a1dc4f1ee749a4f0d3c52ce2deefe7c32fae00
HP <HP@DESKTOP-R9VPA0E>
2025-06-08 08:32:20 +0530

    concat() is the String class Method and its takes the string as parameter and its return the string.(combing 2 given string)

---
+++ Core_JAVA_Programs/src/com/krush/string_class_methods/ConcatMethodDemo.java
@@ -0,0 +1,21 @@
+package com.krush.string_class_methods;
+
+public class ConcatMethodDemo {
+	
+	
+	public static void main(String args[])
+	{
+		
+		String s1= "Country ";
+		String s2 ="INDIA";
+		
+		String s3=" is Grate!!";
+		
+		
+		System.out.println(s1.concat(s2).concat(s3));//its called method chaining
+	// concat() is the String class Method and its takes the string as parameter and its return the string.(combing 2 given string)
+		
+		
+	}
+
+}
