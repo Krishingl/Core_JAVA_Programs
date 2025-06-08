8462749a77053834d3a2420a94dc52144d9ca81d
krrishnaingle2001.gmail.com <krishnaingle2001@gmail.com>
2025-06-08 14:47:14 +0530

    its use to remove the white space before String and ending of the String but, not remove the spaces middle of string white spaces.

---
+++ Core_JAVA_Programs/src/com/krush/string_class_methods/TrimMethod.java
@@ -0,0 +1,18 @@
+package com.krush.string_class_methods;
+
+public class TrimMethod {
+	public static void main(String[] args) {
+		
+		String s1="    krushna   ingle    ";
+
+	 System.out.println(s1.trim());//krushna   ingle
+	 
+	 
+	 
+	 //its use to remove the white space before String and ending of the String but, not remove the spaces middle of string white spaces.
+	 
+	 
+	 
+	}
+
+}
