ea40a47845b1dda4913f1a4cd56761e912683a56
krrishnaingle2001.gmail.com <krishnaingle2001@gmail.com>
2025-06-08 15:38:29 +0530

    the lastIndexOf method is finds the Last means (Right to left First) Occurrence and that will return.

---
+++ Core_JAVA_Programs/src/com/krush/string_class_methods/LastIndexOfMethod.java
@@ -0,0 +1,22 @@
+package com.krush.string_class_methods;
+
+public class LastIndexOfMethod {
+	public static void main(String[] args) {
+		//the lastIndexOf method is finds the Last means (Right to left First) Occurrence and that will return.
+		String str="Oracal is DBMS tool its Use to Store our day to day Transtion data/Information"
+				+ "its support to sql language to communicate our data base.";
+		
+		int lastIndex=str.lastIndexOf("language");
+		System.out.println(str+"lastIndex of \"language\" is : "+lastIndex);
+		
+		
+		System.out.println("Total lenght of String is : "+str.length());
+		
+		
+		
+		
+		
+		
+	}
+
+}
