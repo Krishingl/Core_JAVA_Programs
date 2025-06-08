6068f93591d949df13a7c925941264a27e7b3e79
krrishnaingle2001.gmail.com <krishnaingle2001@gmail.com>
2025-06-08 12:06:29 +0530

    replace() method are overloaded method in the String class. that can be use to replace particular character and also the particular part of string. return type is String.

---
+++ Core_JAVA_Programs/src/com/krush/string_class_methods/ReplaceMethodDemo.java
@@ -0,0 +1,21 @@
+package com.krush.string_class_methods;
+
+public class ReplaceMethodDemo {
+	
+	public static void main(String[] args) {
+		
+		
+		    String s1= "Rama";
+		    
+		    System.out.println("Here Replace character : "+s1.replace('R','U'));
+		
+	
+		
+		String s2 = "Manager";
+		
+		
+		System.out.println("Here replace String inside sub String  : "+s2.replace("Man","Dam"));
+		
+	}
+
+}
