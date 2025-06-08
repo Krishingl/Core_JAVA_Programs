70800d55eb4f4bd00fd33bba7ecdd23be0761e31
krrishnaingle2001.gmail.com <krishnaingle2001@gmail.com>
2025-06-08 15:40:05 +0530

    done

--- Core_JAVA_Programs/src/com/krush/string_class_methods/LastIndexOfMethod.java
+++ Core_JAVA_Programs/src/com/krush/string_class_methods/LastIndexOfMethod.java
@@ -2,7 +2,7 @@ package com.krush.string_class_methods;
 
 public class LastIndexOfMethod {
 	public static void main(String[] args) {
-		//the lastIndexOf method is finds the Last means (Right to left First) Occurrence and that will return.
+		//the lastIndexOf method is finds the Last means (Right to left First) Occurrence and that will 
 		String str="Oracal is DBMS tool its Use to Store our day to day Transtion data/Information"
 				+ "its support to sql language to communicate our data base.";
 		
