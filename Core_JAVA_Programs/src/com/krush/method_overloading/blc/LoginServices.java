9337b27b94f4ccbce3aa5c52d03bc92b2978f8f2
krrishnaingle2001.gmail.com <krishnaingle2001@gmail.com>
2025-06-15 19:29:40 +0530

    two option done one pending

---
+++ Core_JAVA_Programs/src/com/krush/method_overloading/blc/LoginServices.java
@@ -0,0 +1,61 @@
+package com.krush.method_overloading.blc;
+
+public class LoginServices {
+	
+	
+	
+	public void login(String emailId, String password ) {
+		
+		
+		if(emailIdValidation(emailId)&&  validationPassword(password))
+		{
+			System.out.println("Success fully Logined With Email..");
+		}else
+			System.err.println(" <!>Invalid Email or Password Please Enter Valid....");
+		
+	}
+	
+	public void login(String mobileNo , int OTP) {
+		
+		     if(mobileNumberValidation(mobileNo)&& OTPValidation(OTP)) {
+		    	 
+		    	 System.out.println("Success fully Logined With MobileNumber..");
+				}else
+					System.err.println(" <!>Invalid MobileNumber or OTP Please Enter Valid....");
+				
+	}
+	
+	
+	
+	private  boolean  emailIdValidation(String emailId)
+	{
+		if(emailId.contains("gmail.com")) {
+			return true;
+		}
+		return false;
+		
+	}
+     private boolean validationPassword(String password) 
+     {
+    	  if(password.equals("MOL3")) {
+    		  
+    		return true;
+    	  }
+    	 
+    	return false;
+     }
+     
+     
+    private boolean mobileNumberValidation( String mobileNo) {
+    	
+    	if(mobileNo.length()==10) {
+    		return true;
+    	}else return false;
+    }
+    private boolean OTPValidation(int OTP) {
+    	if(OTP==1111) {
+    		return true;
+    	}else 
+    		return false;
+    }
+}
