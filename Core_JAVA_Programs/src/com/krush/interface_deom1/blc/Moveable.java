fde700af91ef3adca746f9ebff8d48fa3af0f5b6
krrishnaingle2001.gmail.com <krishnaingle2001@gmail.com>
2025-06-23 21:46:45 +0530

    done

--- Core_JAVA_Programs/src/com/krush/interface_deom1/blc/Moveable.java
+++ Core_JAVA_Programs/src/com/krush/interface_deom1/blc/Moveable.java
@@ -1,31 +1,14 @@
-package com.krush.interface_deom1;
+package com.krush.interface_deom1.blc;
 
 
-sealed  abstract interface Moveable {
-	     int x=120;  //thats by default public final static field 
+public interface Moveable 
+{
+	     int MAX_SPEED=120;  //thats by default public final static field 
 	    void  run();   //this method is public +abstract method
+	    
+	    
+	    
 
 }
 
 
-
-final class Car implements Moveable{
-	
-	
-public  void run()
-         {
-	        System.out.println("Car is runnig");
-	       
-	    }
-	
-}
-final class Bike implements Moveable{
-	
-	
-public  void run()
-         {
-	        System.out.println("Bike is Running ");
-	       
-	    }
-	
-}
---
+++ Core_JAVA_Programs/src/com/krush/interface_deom1/elc/Bike.java
@@ -0,0 +1,14 @@
+package com.krush.interface_deom1.elc;
+
+import com.krush.interface_deom1.blc.Moveable;
+
+final class Bike implements Moveable{
+	
+	
+public  void run()
+         {
+	        System.out.println("Bike is Running with "+MAX_SPEED+"KM/PH");
+	       
+         }
+	
+}
