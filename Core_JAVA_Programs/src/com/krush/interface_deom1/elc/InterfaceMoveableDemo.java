816198f90297c0e0fdc4a1d21c30b72050946530
krrishnaingle2001.gmail.com <krishnaingle2001@gmail.com>
2025-06-23 21:46:23 +0530

    done

--- Core_JAVA_Programs/src/com/krush/interface_deom1/elc/InterfaceMoveableDemo.java
+++ Core_JAVA_Programs/src/com/krush/interface_deom1/elc/InterfaceMoveableDemo.java
@@ -1,11 +1,15 @@
 package com.krush.interface_deom1.elc;
+import com.krush.interface_deom1.blc.Moveable;
+import com.krush.method_overloading.blc.Car;
 
 public class InterfaceMoveableDemo {
 	
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
+		Moveable car =new Car();
 		
-		
+		   car.run();
 	           
 	}
 
