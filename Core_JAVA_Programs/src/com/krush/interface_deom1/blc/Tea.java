664b8279f8ba8ebd8993701c96eed5be1bd2e509
krrishnaingle2001.gmail.com <krishnaingle2001@gmail.com>
2025-06-24 07:40:39 +0530

    Tea class Ready

---
+++ Core_JAVA_Programs/src/com/krush/interface_deom1/blc/Tea.java
@@ -0,0 +1,10 @@
+package com.krush.interface_deom1.blc;
+
+public final class Tea implements HotDrink {
+	
+	
+	public void prepare() {
+		System.out.println("Prepareing Hydrabad Irani Tea ...");
+	}
+
+}
