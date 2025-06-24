09094b4ff2e0a24ea5610372534b0e5f7dddb6f2
krrishnaingle2001.gmail.com <krishnaingle2001@gmail.com>
2025-06-24 07:41:07 +0530

    Coffee class is ready

---
+++ Core_JAVA_Programs/src/com/krush/interface_deom1/blc/Coffee.java
@@ -0,0 +1,10 @@
+package com.krush.interface_deom1.blc;
+
+public final class Coffee implements HotDrink {
+	
+	
+	public void prepare() {
+		System.out.println("Preparing Filter Coffee...");
+	}
+
+}
