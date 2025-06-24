de1bc3becd20a4c8f5f3bd76b735addfacc24464
krrishnaingle2001.gmail.com <krishnaingle2001@gmail.com>
2025-06-24 06:09:59 +0530

    Interface here all the biusness rules are metioned

---
+++ Core_JAVA_Programs/src/com/krush/interface_deom1/blc/Banking.java
@@ -0,0 +1,14 @@
+package com.krush.interface_deom1.blc;
+
+public abstract  interface Banking {
+
+	  public void diposit(double balance);
+	  public void withdrow(double balance);
+	  
+	  public abstract double getBalance();
+	  
+	
+	
+	
+	     
+}
