30dd77374f5e6b6379058b260d390a785c5017db
krrishnaingle2001.gmail.com <krishnaingle2001@gmail.com>
2025-06-24 07:43:45 +0530

    elc class demo on loose coupling if in feuture we want add new class like Boost then its simple by uesing loose couping maitanace is very low.

---
+++ Core_JAVA_Programs/src/com/krush/interface_deom1/elc/LooseCouplingWithInterfaceHotDrink.java
@@ -0,0 +1,19 @@
+package com.krush.interface_deom1.elc;
+
+import com.krush.interface_deom1.blc.*;
+
+public class LooseCouplingWithInterfaceHotDrink {
+	
+	
+	
+	public static void main(String[] args) {
+		    
+		    Restuarent.prepareHotDrink(new Tea());
+
+		    Restuarent.prepareHotDrink(new Coffee());
+		    
+		    Restuarent.prepareHotDrink(new Coffee());
+		
+	}
+
+}
