292d0332d7068874d4540d138e866d21f0ededfe
krrishnaingle2001.gmail.com <krishnaingle2001@gmail.com>
2025-06-25 06:55:15 +0530

    Vehicle are class are devloped with befor java 8 features

---
+++ Core_JAVA_Programs/src/com/krush/java_8_features/interface_default_method/blc/Vehicle.java
@@ -0,0 +1,13 @@
+package com.krush.java_8_features.interface_default_method.blc;
+
+public interface Vehicle {
+
+	int MAX_SPEED=120;
+	
+	     void run();
+	     void horn();
+	     
+	     
+	     
+	     
+}
