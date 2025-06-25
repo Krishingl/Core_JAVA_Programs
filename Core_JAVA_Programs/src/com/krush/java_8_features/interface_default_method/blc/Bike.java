ee8090a4e43b248e6888f9c13604f887491706c4
krrishnaingle2001.gmail.com <krishnaingle2001@gmail.com>
2025-06-25 06:56:45 +0530

    Car And Bike are the implimenter classes.

---
+++ Core_JAVA_Programs/src/com/krush/java_8_features/interface_default_method/blc/Bike.java
@@ -0,0 +1,15 @@
+package com.krush.java_8_features.interface_default_method.blc;
+
+public class Bike {
+	
+	
+	public void run() {
+		
+		System.out.println("Bike Is running.");
+	}
+      
+public void horn() {
+		
+		System.out.println("Bike Has a Horn.");
+	}
+}
