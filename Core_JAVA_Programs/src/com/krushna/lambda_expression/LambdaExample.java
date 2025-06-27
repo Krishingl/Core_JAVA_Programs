11ced2cedf12446e2c6032175afb547520797600
krrishnaingle2001.gmail.com <krishnaingle2001@gmail.com>
2025-06-27 20:39:01 +0530

    formatted

--- Core_JAVA_Programs/src/com/krushna/lambda_expression/LambdaExample.java
+++ Core_JAVA_Programs/src/com/krushna/lambda_expression/LambdaExample.java
@@ -3,36 +3,27 @@ package com.krushna.lambda_expression;
 import java.util.Scanner;
 
 @FunctionalInterface
-interface Predictable{
-	
+interface Predictable {
+
 	boolean Predict(Integer num);
 
-	
 }
 
 //Write a Program take user input and check its illigible for voting or not
 public class LambdaExample {
-	
+
 	public static void main(String[] args) {
-		
-		
-		
-		Scanner sc= new Scanner(System.in);
-		
+
+		Scanner sc = new Scanner(System.in);
+
 		System.out.print("Enter your Age : ");
 		Integer ag = Integer.parseInt(sc.nextLine());
-		
-	      Predictable pred =age-> age>=18;
-           
-	      
-	      System.out.println(" Is person illegible are Voteing ? "+pred.Predict(ag));
-		
+
+		Predictable pred = age -> age >= 18;
+
+		System.out.println(" Is person illegible are Voteing ? " + pred.Predict(ag));
+
 		sc.close();
 	}
-	
-	
-	
-	
-	
 
 }
