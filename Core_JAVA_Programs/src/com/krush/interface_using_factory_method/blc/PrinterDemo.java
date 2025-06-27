a85b8f83b0c28280895000b8830997ca1eb4d7a3
krrishnaingle2001.gmail.com <krishnaingle2001@gmail.com>
2025-06-27 06:31:02 +0530

    factory method program

--- Core_JAVA_Programs/src/com/krush/interface_using_factory_method/blc/PrinterDemo.java
+++ Core_JAVA_Programs/src/com/krush/interface_using_factory_method/blc/PrinterDemo.java
@@ -4,20 +4,19 @@ public class PrinterDemo implements Printer {
 
 	@Override
 	public void print() {
-		   
+
 		System.out.println("Printing PrinterDome Class Inside");
-		
+
 	}
+
 	public Printer getPrinter() {
 		return new Printer() {
 
 			@Override
 			public void print() {
-				System.out.println("Printing PrinterDome Anonymous Inner Class Inside Class Inside");				
+				System.out.println("Printing PrinterDome Anonymous Inner Class Inside Class Inside");
 			}
-			
-			
-			
+
 		};
 	}
 
