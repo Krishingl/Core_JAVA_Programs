4959ded72b2ceba95cffe551e4e7d49201c3fe79
krrishnaingle2001.gmail.com <krishnaingle2001@gmail.com>
2025-06-27 22:12:05 +0530

    done

---
+++ Core_JAVA_Programs/src/com/krushna/interface_predefine_functions/NumberIsEvenOrNot.java
@@ -0,0 +1,24 @@
+package com.krushna.interface_predefine_functions;
+import java.util.Scanner;
+import java.util.function.Predicate;
+
+public class NumberIsEvenOrNot {
+
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		
+		
+		Predicate<Integer> pd= num-> num%2==0 ;
+		  
+		
+		System.out.print("Enter The number : ");
+		
+		int num = sc.nextInt();
+		System.out.println("Given numeber "+num+"Is even ? "+pd.test(num));
+		
+		
+		
+sc.close();
+	}
+
+}
