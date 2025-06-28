a999afa8a6403df017cd60afd9156ac07ca06e22
krrishnaingle2001.gmail.com <krishnaingle2001@gmail.com>
2025-06-29 05:02:47 +0530

    EMail Id is correct or not.

---
+++ Core_JAVA_Programs/src/com/krushna/interface_predefine_functions/function/FunctionDemo3.java
@@ -0,0 +1,25 @@
+package com.krushna.interface_predefine_functions.function;
+
+import java.util.Scanner;
+import java.util.function.Function;
+
+public class FunctionDemo3
+{
+	//find given String having "@gamil.com" or not?
+	public static void main(String args[]) 
+	{
+		Function<String,Boolean> fn1= (str)-> str.contains("@gmail.com");
+		
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter your Email Id : ");
+		String emailId= sc.next();
+		
+		System.out.println("Is EmailId  Correct ? "+ fn1.apply(emailId));
+		
+		sc.close();
+        		
+	}
+
+}
