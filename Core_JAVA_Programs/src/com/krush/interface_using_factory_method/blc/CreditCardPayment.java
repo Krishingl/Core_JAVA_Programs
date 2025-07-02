a218d41e1bb0ff59f2cbf978b1601c0b01e04f92
krrishnaingle2001.gmail.com <krishnaingle2001@gmail.com>
2025-07-02 22:57:58 +0530

    done

--- Core_JAVA_Programs/src/com/krush/interface_using_factory_method/blc/CreditCardPayment.java
+++ Core_JAVA_Programs/src/com/krush/interface_using_factory_method/blc/CreditCardPayment.java
@@ -1,19 +1,17 @@
 package com.krush.interface_using_factory_method.blc;
 
-
-
 public class CreditCardPayment implements Payment {
 
 	@Override
 	public void processPayment() {
 		System.out.println("Processing credit card payment...");
-		
+
 	}
 
 	@Override
 	public void applyDiscount() {
 		System.out.println("Applying 10% discount for credit card users.");
-		
+
 	}
 
 }
