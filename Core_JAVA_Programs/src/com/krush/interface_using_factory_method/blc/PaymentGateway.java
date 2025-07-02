040b88751ba26457d21a5b207e0468e705cd9140
krrishnaingle2001.gmail.com <krishnaingle2001@gmail.com>
2025-07-02 22:57:27 +0530

    formated

--- Core_JAVA_Programs/src/com/krush/interface_using_factory_method/blc/PaymentGateway.java
+++ Core_JAVA_Programs/src/com/krush/interface_using_factory_method/blc/PaymentGateway.java
@@ -1,20 +1,17 @@
 package com.krush.interface_using_factory_method.blc;
 
 public class PaymentGateway {
-	
-	
-	public Payment initiatePayment(Payment payment) //Loose coupling
+
+	public Payment initiatePayment(Payment payment) // Loose coupling
 	{
 		System.out.println("Initiating Credit Card payment.");
-		
-		if(payment instanceof CreditCardPayment) {
+
+		if (payment instanceof CreditCardPayment) {
 			return payment;
-		}else {
+		} else {
 			return payment;
 		}
-		
-		
-		
+
 	}
 
 }
