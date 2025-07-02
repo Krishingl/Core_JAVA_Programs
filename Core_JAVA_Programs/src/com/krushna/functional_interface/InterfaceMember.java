f0316f85958f651e710e9149a4951e694373af96
krrishnaingle2001.gmail.com <krishnaingle2001@gmail.com>
2025-07-02 20:26:11 +0530

    we cannot write default method same Signature of object class method because they all ready available in the class from Object class

---
+++ Core_JAVA_Programs/src/com/krushna/functional_interface/InterfaceMember.java
@@ -0,0 +1,16 @@
+package com.krushna.functional_interface;
+
+interface Alpha
+{
+//	default String toString(){//error
+//
+//		//A default method cannot override a method from java.lang.Object 
+//	return null; 	
+//}
+}
+public class InterfaceMember implements Alpha {
+   
+	public String  toString() {
+		return "Ambiguty issue because default method of interface and concrete method toString from Object class both are available in implementer class that reason its give to error and other reason in between default method and concrete method more priority have default method then in that Object class method are first available its execute then then default method we will give get error";
+	}
+}
