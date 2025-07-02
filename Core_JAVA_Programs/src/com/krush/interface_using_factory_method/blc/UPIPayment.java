d527c9e37a9868af42dd90e0633ee899d8a07340
krrishnaingle2001.gmail.com <krishnaingle2001@gmail.com>
2025-07-02 22:58:44 +0530

    formated

--- Core_JAVA_Programs/src/com/krush/interface_using_factory_method/blc/UPIPayment.java
+++ Core_JAVA_Programs/src/com/krush/interface_using_factory_method/blc/UPIPayment.java
@@ -4,14 +4,13 @@ public class UPIPayment implements Payment {
 
 	public void processPayment() {
 		System.out.println("Processing UPI payment...");
-		
+
 	}
 
 	@Override
 	public void applyDiscount() {
 		System.out.println("Applying ₹100 cashback for UPI users.");
-		
-		
+
 	}
 
 }
