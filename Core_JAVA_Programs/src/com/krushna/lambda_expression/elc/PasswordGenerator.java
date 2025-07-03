0b64768e5929b80c1e9a4654da2506cf5e7d510c
krrishnaingle2001.gmail.com <krishnaingle2001@gmail.com>
2025-07-03 18:01:37 +0530

    proper formated

--- Core_JAVA_Programs/src/com/krushna/lambda_expression/elc/PasswordGenerator.java
+++ Core_JAVA_Programs/src/com/krushna/lambda_expression/elc/PasswordGenerator.java
@@ -1,44 +1,37 @@
 package com.krushna.lambda_expression.elc;
+
 import java.util.Random;
 import java.util.function.Supplier;
+
 public class PasswordGenerator {
 
-	public static void main(String[] args) 
-	{
-		  
-		 String upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-		 String lower = "abcdefghijklmnopqrstuvwxyz";
-		 String digits = "0123456789";
-		 String special = "@#$%!&*";
-		 
-		 
-		 String totalCharInPWD = upper + lower + digits+ special ;
-		 
-		 int pwdLength= 8;
-		 
-		 
-		 
-		 Supplier<String>  passwordSupplier =  ()->{
-			 
-			 String password = "";
-			 
-			 for(int i = 0 ;  i < pwdLength ; i++)
-			 {
-				   
-				 Random r= new Random();
-					int num= r.nextInt(totalCharInPWD.length());
-			
-					
-					password += totalCharInPWD.charAt(num);
-					
-				 
-	
-			 }
-			 return password;
-		 };
-		
-	
-	System.out.println("Generated Password: "+  passwordSupplier.get());
+	public static void main(String[] args) {
+
+		String upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		String lower = "abcdefghijklmnopqrstuvwxyz";
+		String digits = "0123456789";
+		String special = "@#$%!&*";
+
+		String totalCharInPWD = upper + lower + digits + special;
+
+		int pwdLength = 8;
+
+		Supplier<String> passwordSupplier = () -> {
+
+			String password = "";
+
+			for (int i = 0; i < pwdLength; i++) {
+
+				Random r = new Random();
+				int num = r.nextInt(totalCharInPWD.length());
+
+				password += totalCharInPWD.charAt(num);
+
+			}
+			return password;
+		};
+
+		System.out.println("Generated Password: " + passwordSupplier.get());
 	}
 
 }
