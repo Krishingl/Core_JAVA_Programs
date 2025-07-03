917b5ba69a9efdef13f33a31e7789821d7e6c222
krrishnaingle2001.gmail.com <krishnaingle2001@gmail.com>
2025-07-03 21:55:22 +0530

    BufferedReader is used to read the input from runtime in String format only they have one method only readLine()

---
+++ Core_JAVA_Programs/src/test/Test.java
@@ -0,0 +1,16 @@
+package test;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+public class Test {
+	
+	
+
+	public static void main(String args[])  throws IOException
+	{
+		BufferedReader br= new BufferedReader(new InputStreamReader(System.in) );
+	 String sr=	br.readLine();
+	 System.out.println(sr);
+			
+	}
+}
