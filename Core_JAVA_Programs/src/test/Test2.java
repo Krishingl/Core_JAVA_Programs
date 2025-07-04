8a9f0275cf82e681608017f17b3a9accef1e9b45
krrishnaingle2001.gmail.com <krishnaingle2001@gmail.com>
2025-07-04 07:29:28 +0530

    ClassNotFoundException

---
+++ Core_JAVA_Programs/src/test/Test2.java
@@ -0,0 +1,15 @@
+package test;
+
+public class Test2 {
+
+	public static void main(String[] args) throws ClassNotFoundException {
+	    //System.out.println(10/0);
+	    
+	   java.lang.ArithmeticException a = new   java.lang.ArithmeticException();
+	    
+	    
+	  System.out.println(  Class.forName(null));
+
+	}
+
+}
