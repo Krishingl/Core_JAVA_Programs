2f02b6c69c446448f9d64a3d75f6bf3814a4c128
krrishnaingle2001.gmail.com <krishnaingle2001@gmail.com>
2025-07-11 22:22:31 +0530

    Done

--- Core_JAVA_Programs/src/com/krushna/custom/exception/CustomCheckedException.java
+++ Core_JAVA_Programs/src/com/krushna/custom/exception/CustomCheckedException.java
@@ -7,7 +7,7 @@ class InvalidAgeException extends Exception {
 	private static final long serialVersionUID = 1L;
 
 	public InvalidAgeException() {
-
+    
 	}
 
 	public InvalidAgeException(String errorMessage) {
