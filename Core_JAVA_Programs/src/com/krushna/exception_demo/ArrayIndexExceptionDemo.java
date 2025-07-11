b26328a305aa1bb89431af509b9eeb6587d3761c
krrishnaingle2001.gmail.com <krishnaingle2001@gmail.com>
2025-07-11 17:33:26 +0530

    done

--- Core_JAVA_Programs/src/com/krushna/exception_demo/ArrayIndexExceptionDemo.java
+++ Core_JAVA_Programs/src/com/krushna/exception_demo/ArrayIndexExceptionDemo.java
@@ -1,34 +1,32 @@
 package com.krushna.exception_demo;
 
- import java.util.Scanner;
- public class ArrayIndexExceptionDemo{
-    public static void main(String args[]){
-          int a[] = new int[2];
-   a[0] = 10;
-   a[1] = 20;
+import java.util.Scanner;
 
-    Scanner sc= new Scanner(System.in);
-      
-  try(sc){
-    
-          System.out.println(" enter an index value : ");
-        int n= sc.nextInt();
+public class ArrayIndexExceptionDemo {
+	public static void main(String args[]) {
+		int a[] = new int[2];
+		a[0] = 10;
+		a[1] = 20;
 
-        System.out.print(a[n]);
-  }catch(ArrayIndexOutOfBoundsException e){
-    System.out.println(  e.getMessage());
-System.out.println("-------------------------------");
-    System.out.println( e.toString());
-    System.out.println("-------------------------------");
-    e.printStackTrace();
-    System.out.println("-------------------------------");
-    System.out.println( "Invalid Index");
+		Scanner sc = new Scanner(System.in);
 
-    
-  }
+		try (sc) {
 
+			System.out.println(" enter an index value : ");
+			int n = sc.nextInt();
 
-    }
+			System.out.print(a[n]);
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println(e.getMessage());
+			System.out.println("-------------------------------");
+			System.out.println(e.toString());
+			System.out.println("-------------------------------");
+			e.printStackTrace();
+			System.out.println("-------------------------------");
+			System.out.println("Invalid Index");
 
+		}
+
+	}
 
 }
