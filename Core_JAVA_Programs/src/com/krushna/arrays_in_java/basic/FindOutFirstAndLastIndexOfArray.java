46cccbca8cb7ef813045e9d4df3ce272de889fe9
krrishnaingle2001.gmail.com <krishnaingle2001@gmail.com>
2025-07-12 20:14:08 +0530

    Array are Index based- here we are storing and accessing the elements based on the index only. array index starts with O.

--- Core_JAVA_Programs/src/com/krushna/arrays_in_java/basic/FindOutFirstAndLastIndexOfArray.java
+++ Core_JAVA_Programs/src/com/krushna/arrays_in_java/basic/FindOutFirstAndLastIndexOfArray.java
@@ -2,18 +2,17 @@ package com.krushna.arrays_in_java.basic;
 
 public class FindOutFirstAndLastIndexOfArray {
 
-	public static void main(String[] args)
-	{
-		int [] arrs = {10,20,30,60,20};
-		
+	public static void main(String[] args) {
+		int[] arrs = { 10, 20, 30, 60, 20 };
+
 		// Size/ length / number of elements in given array
-		System.out.println("Size of Given Array Is : "+arrs.length);
-		
-         //First index
-		System.out.println("The First index Of Array element is :"+ arrs[0]);
-		
-		//Last Index 
-		System.out.println("The Last index Of Array element is :"+ arrs[arrs.length-1]);
+		System.out.println("Size of Given Array Is : " + arrs.length);
+
+		// First index
+		System.out.println("The First index Of Array element is :" + arrs[0]);
+
+		// Last Index
+		System.out.println("The Last index Of Array element is :" + arrs[arrs.length - 1]);
 	}
 
 }
