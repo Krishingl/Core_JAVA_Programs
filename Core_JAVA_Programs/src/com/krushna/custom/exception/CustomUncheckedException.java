2ccb2af2a0a8b6fc6b3567ca238038b8455c8d57
krrishnaingle2001.gmail.com <krishnaingle2001@gmail.com>
2025-07-12 07:06:48 +0530

    Custom or user-defiend UnChecked Exception

--- Core_JAVA_Programs/src/com/krushna/custom/exception/CustomUncheckedException.java
+++ Core_JAVA_Programs/src/com/krushna/custom/exception/CustomUncheckedException.java
@@ -1,54 +1,46 @@
 package com.krushna.custom.exception;
+
 import java.util.Scanner;
+
 //@SuppressWarnings("serial")
-class  GreaterMarksException extends RuntimeException
-{
+class GreaterMarksException extends RuntimeException {
 	private static final long serialVersionUID = 1L;
-	
-	
-	public GreaterMarksException() 
-	{
-		
+
+	public GreaterMarksException() {
+
 	}
-public GreaterMarksException(String errorMessage) 
-{
-	super(errorMessage);
-		
+
+	public GreaterMarksException(String errorMessage) {
+		super(errorMessage);
+
 	}
-	
-	
+
 }
 
+public class CustomUncheckedException {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 
+		try (sc) {
+			System.out.print("Enter Marks : ");
+			int marks = sc.nextInt();
+			validMarks(marks);
 
-public class CustomUncheckedException
-{
- public static void main(String[] args)
- {
-	 Scanner sc =new Scanner(System.in);
-	 
-	 try(sc){
-		 System.out.print("Enter Marks : ");
-		 int marks= sc.nextInt();
-		 validMarks(marks);
-		 
-	 }catch(GreaterMarksException e) {
-		 
-		 System.out.println(e.getMessage());
-		 System.out.println("======================================");
-		 System.out.println(e.toString());
-		 System.out.println("======================================");
-		 e.printStackTrace();
-	 }
-	 
-	 
-	
-}
- 
- public static void validMarks(int marks) {
-	 if(marks>100) {
-		 throw new  GreaterMarksException("Invalid Marks...");
-	 }
-	 System.out.println("Your marks is : "+marks);
- }
+		} catch (GreaterMarksException e) {
+
+			System.out.println(e.getMessage());
+			System.out.println("======================================");
+			System.out.println(e.toString());
+			System.out.println("======================================");
+			e.printStackTrace();
+		}
+
+	}
+
+	public static void validMarks(int marks) {
+		if (marks > 100) {
+			throw new GreaterMarksException("Invalid Marks...");
+		}
+		System.out.println("Your marks is : " + marks);
+	}
 }
