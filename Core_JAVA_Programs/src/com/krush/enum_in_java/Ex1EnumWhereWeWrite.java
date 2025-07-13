133570fe344db5a95b2bd679a3c9d9436b83afb4
krrishnaingle2001.gmail.com <krishnaingle2001@gmail.com>
2025-07-13 19:17:14 +0530

    done

---
+++ Core_JAVA_Programs/src/com/krush/enum_in_java/Ex1EnumWhereWeWrite.java
@@ -0,0 +1,30 @@
+//WAP to show Where we can write a enum.
+//--> we can write the enum in side class , outside class and inside method
+//1. write a enum Outside the class
+package com.krush.enum_in_java;
+
+enum Colours{
+	RED,BLUE,GREEN,YELLOW,WHITE,BLACK,ORANGE,PINK;//HERE SEMECOLON OPTIONAL
+}
+
+public class Ex1EnumWhereWeWrite {
+  //enum inside the class
+	
+	enum Months{
+		JANUARY,FEBRUARY,MARCH,APRIL,MAY,JUNE,JULY,AUGUST,SEPTEMBER,NOVEMBER,DECEMBER;
+		
+	}
+	public static void main(String[] args) {
+		
+    
+	// enum inside the method
+
+		enum Season{
+			Winter,SPRING,SUMMER,AUTUMN
+		}
+		  
+		  
+		  
+	}
+
+}
