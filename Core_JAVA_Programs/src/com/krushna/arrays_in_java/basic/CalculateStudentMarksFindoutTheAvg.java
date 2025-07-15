1f434dd2ce43889a36134c896e0c1ba8a6e41030
krrishnaingle2001.gmail.com <krishnaingle2001@gmail.com>
2025-07-15 06:47:50 +0530

    Done

---
+++ Core_JAVA_Programs/src/com/krushna/arrays_in_java/basic/CalculateStudentMarksFindoutTheAvg.java
@@ -0,0 +1,56 @@
+package com.krushna.arrays_in_java.basic;
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+public class CalculateStudentMarksFindoutTheAvg {
+
+	public static void main(String[] args) 
+	{
+          
+		Scanner sc = new Scanner(System.in);
+		try(sc){
+	    System.out.print("Enter the Total Subjects : ");
+		int totalSubjects= sc.nextInt();	
+		
+		int [] subjects= new int[totalSubjects];
+		
+		for(int i=0 ; (i < subjects.length) ;i++)
+		{
+			  System.out.print("Enter the "+(i+1)+" Subject Marks : " );
+			  subjects[i]=sc.nextInt();
+			  
+			
+		}
+		
+		  
+		   int totalMarks=ArraysCustomMethods.sum(subjects);
+		   System.out.println("Total Marks is : "+totalMarks);
+	       double average = ArraysCustomMethods.avg(subjects);
+	       System.out.println("Average is  : "+average);
+	     
+	     
+	    
+		}catch(InputMismatchException ex) 
+		{
+			System.err.println(" Enter proper Marks In integer format only ");
+		}catch(ArrayIndexOutOfBoundsException e) {
+			System.err.println(e.getMessage());
+		}catch(ArrayStoreException e)
+		{
+			System.err.print("invalid Value Storing ");
+		} catch (NegativeArraySizeException e) {
+			System.err.println("Array Size Must Be Positive Integer");
+		}
+		
+	    
+	
+		   
+		
+		
+		
+		
+		
+
+	}
+
+}
