ee093c6bfe27c5210fc61e9cc2f9f305749e310b
krrishnaingle2001.gmail.com <krishnaingle2001@gmail.com>
2025-07-20 22:23:12 +0530

    done

--- Core_JAVA_Programs/src/test/ThreadDemo.java
+++ Core_JAVA_Programs/src/test/ThreadDemo.java
@@ -1,12 +1,16 @@
 package test;
 
-public class ThreadDemo {
+public class ThreadDemo  {
 	
 	
 public static void main(String[] args) {
-	        
+	        int a= 10/0;
 	
 	    Thread.currentThread();
+	    
+	    
 }
 
 }
+
+
