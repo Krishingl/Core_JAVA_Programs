1f132cc90aa998b27d37a44c51003b61106e4de3
krrishnaingle2001.gmail.com <krishnaingle2001@gmail.com>
2025-07-21 18:21:31 +0530

    test done

--- Core_JAVA_Programs/src/com/krushna/exception/thread/basic/CustomThread1.java
+++ Core_JAVA_Programs/src/com/krushna/exception/thread/basic/CustomThread1.java
@@ -4,22 +4,42 @@ package com.krushna.exception.thread.basic;
 {
      @Override
     public void run()
-     {
+     {   
+    	 
+    	 System.out.println("Thread-0 started");
+    	 
+    	 String name1 = new MyThread().currentThread().getName();
+    	 
+    	 System.out.print(name1);
+    	 String name2 = new MyThread().currentThread().getName();
+    	 System.out.print(name2);
     	 
     	 
      }
 }
  
- public class CustomThread1
+ public class CustomThread1 extends Thread
  {
 	 public static void main(String[] args)
 	 {
+
+    	 
+    	 System.out.println(new CustomThread1().currentThread().getName());
 		System.out.println("Main Thread Started ..!!");
 		
 		MyThread mt = new MyThread();
 		mt.start();//Thread class
-		
+		mt.start();
 		System.out.println("Main Thread Ended ..!!");
+		MyThread mt1 = new MyThread();
+		
+		mt1.start();
+		mt1.isDaemon();
+
+		mt1.setName("krushna"+mt1.isAlive());
+	      
+		
+		System.out.print(Thread.activeCount());
 		
 	}
  }
